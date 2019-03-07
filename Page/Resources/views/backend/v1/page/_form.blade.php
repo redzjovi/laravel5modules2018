fdc648f6577336ff19032559b28ab8fdaf5320f5
Jovi <redzjovi@gmail.com>
2019-03-07 16:24:26 +0700

    [Npm] add new component input-file-images.

--- Page/Resources/views/backend/v1/page/_form.blade.php
+++ Page/Resources/views/backend/v1/page/_form.blade.php
@@ -44,72 +44,20 @@
             <label class="col-sm-2" for="content">@lang('cms::cms.image')</label>
             <div class="col-sm-10">
                 @foreach ($model->getMedia('page_image') as $medium)
-                    <div class="image_p input-group">
-                        <img class="img-thumbnail" src="{{ $medium->getFullUrl() }}" style="height: 50px; object-fit: scale-down; width: 50px;" />
-                        <input name="image_id[]" type="hidden" value="{{ $medium->id }}" />
-                        <div class="input-group-append">
-                            <button class="btn btn-secondary btn-sm"
-                                onclick="$(this).parent().parent().remove()"
-                                type="button">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </div>
-                    </div>
+                    <input-file-image-default name="image_id[]" src="{{ $medium->getFullUrl() }}" value="{{ $medium->id }}"></input-file-image-default>
                 @endforeach
 
-                <div class="d-none images_div">
-                    <div class="image_p input-group">
-                        <input accept="image/*" class="form-control form-control-sm" name="image[]" type="file" />
-                        <div class="input-group-append">
-                            <button class="btn btn-secondary btn-sm"
-                                onclick="$(this).parent().parent().remove()"
-                                type="button">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <button class="btn btn-secondary btn-sm"
-                    onclick="$('.image_p:last').clone().insertBefore('.images_div');"
-                    type="button">
-                    <i class="fas fa-plus"></i>
-                </button>
+                <input-file-images name="image[]"></input-file-images>
             </div>
         </div>
         <div class="form-group row">
             <label class="col-sm-2" for="content">@lang('cms::cms.gallery')</label>
             <div class="col-sm-10">
                 @foreach ($model->getMedia('page_gallery') as $medium)
-                    <div class="gallery_p input-group">
-                        <img class="img-thumbnail" src="{{ $medium->getFullUrl() }}" style="height: 50px; object-fit: scale-down; width: 50px;" />
-                        <input name="gallery_id[]" type="hidden" value="{{ $medium->id }}" />
-                        <div class="input-group-append">
-                            <button class="btn btn-secondary btn-sm"
-                                onclick="$(this).parent().parent().remove()"
-                                type="button">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </div>
-                    </div>
+                    <input-file-image-default name="gallery_id[]" src="{{ $medium->getFullUrl() }}" value="{{ $medium->id }}"></input-file-image-default>
                 @endforeach
 
-                <div class="d-none galleries_div">
-                    <div class="gallery_p input-group">
-                        <input accept="image/*" class="form-control form-control-sm" name="gallery[]" type="file" />
-                        <div class="input-group-append">
-                            <button class="btn btn-secondary btn-sm"
-                                onclick="$(this).parent().parent().remove()"
-                                type="button">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <button class="btn btn-secondary btn-sm"
-                    onclick="$('.gallery_p:last').clone().insertBefore('.galleries_div');"
-                    type="button">
-                    <i class="fas fa-plus"></i>
-                </button>
+                <input-file-images name="gallery[]"></input-file-images>
             </div>
         </div>
     </div>
