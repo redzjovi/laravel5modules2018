e4891ae6b7ce02c5c8c68af0c60b48f484aa48c7
Jovi <redzjovi@gmail.com>
2019-03-17 08:17:32 +0700

    [Cms] add LocalizationMiddleware.

---
+++ Cms/Http/Middleware/LocalizationMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\Cms\Http\Middleware;
+
+use Closure;
+
+class LocalizationMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $locale = false;
+        $locales = array_keys(config('cms.locales'));
+
+        if ($request->expectsJson()) {
+            $locale = $request->header('Accept-Language') ? $request->header('Accept-Language') : $locale;
+        } else {
+            $locale = $request->session()->get('locale') ? $request->session()->get('locale') : $locale;
+        }
+
+        if (in_array($locale, $locales)) {
+            app()->setLocale($locale);
+        }
+
+        return $next($request);
+    }
+}
